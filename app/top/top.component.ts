fada276f205cde7253f62157b427488fc894dc79
Yavor Komitov <yavork@gmail.com>
2017-01-07 15:03:10 +0200

    topMovies cache OK

--- app/top/top.component.ts
+++ app/top/top.component.ts
@@ -11,36 +11,12 @@ import { TopService } from './../core/services/top.service';
 export class TopComponent implements OnInit, DoCheck, OnDestroy {
     topMovies: Movie[] = [];
     topMoviesKey: string = 'topMovies';
+    updateCacheDate: string = 'lastUpdate';
 
     constructor(private topService: TopService) { }
 
     ngOnInit() {
-
-        var moviesLastUpdated = localStorage['lastUpdate'];
-        var currentDate = new Date();
-        var currentDateAsSeconds = currentDate.getTime().toString();
-        var cacheReloadDate = new Date(moviesLastUpdated * 1 + 60000);
-
-
-        console.log('current date: ' + currentDateAsSeconds);
-        console.log('last update: ' + moviesLastUpdated);
-        console.log('valid until: ' + cacheReloadDate.getTime().toString());
-        console.log('is valid: ' + (currentDateAsSeconds < cacheReloadDate.getTime().toString()));
-
-        if (localStorage[this.topMoviesKey] && (currentDateAsSeconds < cacheReloadDate.getTime().toString())) {
-            console.log('movies from localStorage');
-            this.topMovies = JSON.parse(localStorage[this.topMoviesKey]);
-            return;
-        } else {
-            if (!(currentDateAsSeconds < cacheReloadDate.getTime().toString())) {
-                console.log('MOVIES RELOADING...');
-            }
-            this.topMovies = [];
-            localStorage.removeItem(this.topMoviesKey);
-            console.log('calling movies service');
-            this.topService.getTop10Ids()
-                .subscribe(res => this.topMovies = res);
-        }
+        this.getMovies();
     }
 
     ngDoCheck() {
@@ -48,13 +24,33 @@ export class TopComponent implements OnInit, DoCheck, OnDestroy {
     }
 
     ngOnDestroy() {
-
         if (!localStorage[this.topMoviesKey]) {
-
-            console.log('movies saved to localStorage');
-
             localStorage.setItem(this.topMoviesKey, JSON.stringify(this.topMovies));
-            localStorage.setItem('lastUpdate', new Date().getTime().toString());
+            localStorage.setItem(this.updateCacheDate, new Date().getTime().toString());
+        }
+    }
+
+    validateMovieCache(): boolean {
+        let minutesToCache = 1;
+        let currentDate = new Date().getTime().toString();
+        let cacheReloadDate = new Date(localStorage[this.updateCacheDate] * 1 + 60000 * minutesToCache);
+        
+        return currentDate < cacheReloadDate.getTime().toString();
+    }
+
+    getMovies() {
+        // If movies are cached in localStorage & the cache is valid
+        if (localStorage[this.topMoviesKey] && this.validateMovieCache()) {
+            this.topMovies = JSON.parse(localStorage[this.topMoviesKey]);
+            return;
+        } else {
+            // clear localStorage & movies array
+            this.topMovies = [];
+            localStorage.removeItem(this.topMoviesKey);
+
+            // update movies
+            this.topService.getTop10Ids()
+                .subscribe(res => this.topMovies = res);
         }
     }
 }
